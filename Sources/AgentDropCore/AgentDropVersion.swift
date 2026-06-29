afe6117ee507004c7e3745c6408a4bbec7d9dafe
Chris Wang <chris.whq@gmail.com>
2026-06-29 15:03:41 +0800

    chore: bump version to 0.2.4

--- Sources/AgentDropCore/AgentDropVersion.swift
+++ Sources/AgentDropCore/AgentDropVersion.swift
@@ -1,5 +1,5 @@
 public enum AgentDropVersion {
-    public static let current = "0.2.3"
-    public static let build = "4"
+    public static let current = "0.2.4"
+    public static let build = "5"
     public static let display = "v\(current) (\(build))"
 }
