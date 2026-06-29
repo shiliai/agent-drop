74eb142cd45bfe7399daa511c0ea133c6bbf2862
johnymoo <johnymoo12@gmail.com>
2026-06-29 15:04:56 +0800

    chore: bump version to 0.2.4

--- Tests/AgentDropCoreTests/AgentDropCoreSmokeTests.swift
+++ Tests/AgentDropCoreTests/AgentDropCoreSmokeTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class AgentDropCoreSmokeTests: XCTestCase {
     func testVersionConstantIsAvailable() {
-        XCTAssertEqual(AgentDropVersion.current, "0.2.3")
-        XCTAssertEqual(AgentDropVersion.build, "4")
-        XCTAssertEqual(AgentDropVersion.display, "v0.2.3 (4)")
+        XCTAssertEqual(AgentDropVersion.current, "0.2.4")
+        XCTAssertEqual(AgentDropVersion.build, "5")
+        XCTAssertEqual(AgentDropVersion.display, "v0.2.4 (5)")
     }
 }
